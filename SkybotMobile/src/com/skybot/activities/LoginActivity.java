32eb815318ea7a5ddb646f52b86a8e86b7b9398c
goraga1 <goraga1@gmail.com>
2013-01-30 14:19:47 +0500

    Login Page and Registration Page

---
+++ SkybotMobile/src/com/skybot/activities/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.skybot.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+	}
+
+	public void onClick(View v) {
+		// Switching to SkybotTabLayoutActivity screen
+		Intent skybottablayoutIntent = new Intent(getApplicationContext(),
+				SkybotTabLayoutActivity.class);
+
+		startActivity(skybottablayoutIntent);
+
+		/**
+		 * REGESTATION
+		 * 
+		 */
+
+		/*
+		 * TextView registerScreen = (TextView)
+		 * findViewById(R.id.link_to_register);
+		 */
+
+		// Listening to register new account link
+		/*
+		 * registerScreen.setOnClickListener(new View.OnClickListener() {
+		 * 
+		 * public void onClick(View v) { // Switching to Register screen Intent
+		 * i = new Intent(getApplicationContext(), RegisterActivity.class);
+		 * startActivity(i); } });
+		 */
+	}
+}
