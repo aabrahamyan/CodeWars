d2060c1e2a6d02f288ba88bf21c670d2ab35a187
Armen Abrahamyan <abrahamyan.armen@gmail.com>
2013-01-30 18:45:31 +0400

    * Added comments on Class name - BackgroundResponseAnalizer.java

--- SkybotMobile/src/com/skybot/serivce/BackgroundResponseAnalizer.java
+++ SkybotMobile/src/com/skybot/serivce/BackgroundResponseAnalizer.java
@@ -2,12 +2,23 @@ package com.skybot.serivce;
 
 import java.util.Map;
 
+/**
+ * Executes new thread for parsing response data
+ * @author aabraham
+ *
+ */
 public class BackgroundResponseAnalizer extends Thread {
 
 	private String serviceName;
 	private String responseData;
 	private Map<String, String> params;
 
+	/**
+	 * 
+	 * @param serviceName
+	 * @param responseData
+	 * @param params
+	 */
 	public BackgroundResponseAnalizer(final String serviceName,
 			final String responseData, final Map<String, String> params) {
 
