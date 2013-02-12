4e11b7f43ed7303af23a14da0993d4d3cee05e4d
goraga1 <goraga1@gmail.com>
2013-02-12 16:36:49 +0500

    login Activity Commit , set login and pass from xml layout , check login and pass.

--- SkybotMobile/src/com/skybot/activities/LoginActivity.java
+++ SkybotMobile/src/com/skybot/activities/LoginActivity.java
@@ -1,11 +1,15 @@
 package com.skybot.activities;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import org.apache.http.NameValuePair;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.skybot.connection.connection.BaseNetworkManager;
 import com.skybot.connection.connection.helper.RequestCreator;
@@ -13,17 +17,15 @@ import com.skybot.connection.connection.helper.RequestHelper;
 import com.skybot.util.Base64Coder;
 import com.skybot.util.Constants;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-
 public class LoginActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
 	}
+
+	EditText username;
+	EditText password;
 
 	public void loginAction(View v) {
 
@@ -33,23 +35,36 @@ public class LoginActivity extends Activity {
 
 		// ------------------- Setting up login request here
 		// ------------------//
-		final String authToken = Base64Coder.encodeRandomBase64(); //"td7b4DquQScIPx9jqs0WSy07YX+AvCjRu/WzdyaCyi0=";
-		final String username = "admin";
-		final String password = "admin";
-		
+		final String authToken = Base64Coder.encodeRandomBase64(); // "td7b4DquQScIPx9jqs0WSy07YX+AvCjRu/WzdyaCyi0=";
+
+		username = (EditText) this.findViewById(R.id.txtUname);
+		password = (EditText) this.findViewById(R.id.txtPwd);
+
 		BaseNetworkManager baseNetworkManager = new BaseNetworkManager();
 
 		RequestCreator creator = new RequestCreator();
-		Map<String, String> params = creator.createAppropriateMapRequest(
-				Constants.AUTH_TOKEN, authToken, Constants.USERNAME, username,
-				Constants.PASSWORD, password, Constants.COMMIT, "Log In");
-		
-		//----------------------- Construct POST DATA ---------------------------//			
-		final RequestHelper reqHelper = new RequestHelper();
-		final List<NameValuePair> paramsList = reqHelper.createPostDataWithKeyValuePair(params);
+		if ((username.getText().toString()).equals(password.getText()
+				.toString())) {
 
-		baseNetworkManager.constructConnectionAndHit("Login Successful",
-				"Login Request Started", paramsList, this, Constants.LOGIN_VIEW,
-				Constants.LOGIN_SERVICE);
+			Map<String, String> params = creator.createAppropriateMapRequest(
+
+			Constants.AUTH_TOKEN, authToken, Constants.USERNAME, username,
+					Constants.PASSWORD, password, Constants.COMMIT, "Log In");
+
+			// ----------------------- Construct POST DATA
+			// ---------------------------//
+			final RequestHelper reqHelper = new RequestHelper();
+			final List<NameValuePair> paramsList = reqHelper
+					.createPostDataWithKeyValuePair(params);
+
+			baseNetworkManager.constructConnectionAndHit("Login Successful",
+					"Login Request Started", paramsList, this,
+					Constants.LOGIN_VIEW, Constants.LOGIN_SERVICE);
+
+		} else {
+			Toast.makeText(LoginActivity.this, "Invalid Login",
+					Toast.LENGTH_LONG).show();
+		}
+
 	}
 }
