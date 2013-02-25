c48886597c4c6496651e96c9ca45f13c7807057f
Arshak Avagyan <arshak.avagyan@sourcio2.com>
2013-02-25 19:02:44 +0500

    Added new Class for counting max value of charts.

---
+++ SkybotMobile/src/com/skybot/charts/GetMaxAxis.java
@@ -0,0 +1,20 @@
+package com.skybot.charts;
+
+public class GetMaxAxis {
+	
+	public double getMax(double[] y) {
+		double maxValue = 0;
+		float maxAxis;
+		for(int i=0; i<y.length;i++){ 
+			maxValue = y[0];
+			if(y[i] > maxValue) { 
+				maxValue = y[i];
+				
+			}
+		}
+		System.out.println(maxValue);
+		maxAxis = (float) Math.ceil(maxValue);
+		return maxAxis;
+	}
+}
+
