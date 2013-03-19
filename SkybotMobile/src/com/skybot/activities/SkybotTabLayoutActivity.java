ffa029f04e5c06b800bcaa22ab87b5dd583a3ade
goraga1 <goraga1@gmail.com>
2013-03-19 11:57:05 +0500

    Push

--- SkybotMobile/src/com/skybot/activities/SkybotTabLayoutActivity.java
+++ SkybotMobile/src/com/skybot/activities/SkybotTabLayoutActivity.java
@@ -2,6 +2,8 @@ package com.skybot.activities;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import com.skybot.util.ServerUtilities;
 import static com.skybot.util.CommonUtilities.DISPLAY_MESSAGE_ACTION;
 import static com.skybot.util.CommonUtilities.EXTRA_MESSAGE;
 import static com.skybot.util.CommonUtilities.SENDER_ID;
@@ -10,6 +12,7 @@ import android.app.TabActivity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
@@ -21,6 +24,7 @@ import android.widget.TabHost.TabSpec;
 import android.widget.Toast;
 
 import com.google.android.gcm.GCMRegistrar;
+
 import com.skybot.activities.delegate.ActionDelegate;
 
 /**
@@ -40,11 +44,69 @@ public class SkybotTabLayoutActivity extends TabActivity implements
 	TextView mDisplay;
 	AsyncTask<Void, Void, Void> mRegisterTask;
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
+		// ----------------------PushNotifications------------------------//
+		checkNotNull(SERVER_URL, "SERVER_URL");
+		checkNotNull(SENDER_ID, "SENDER_ID");
+		// Make sure the device has the proper dependencies.
+		GCMRegistrar.checkDevice(this);
+		// Make sure the manifest was properly set - comment out this line
+		// while developing the app, then uncomment it when it's ready.
+		GCMRegistrar.checkManifest(this);
+		setContentView(R.layout.main);
+		// mDisplay = (TextView) findViewById(R.id.display);
+		registerReceiver(mHandleMessageReceiver, new IntentFilter(
+				DISPLAY_MESSAGE_ACTION));
+		final String regId = GCMRegistrar.getRegistrationId(this);
+		if (regId.equals("")) {
+			// Automatically registers application on startup.
+			GCMRegistrar.register(this, SENDER_ID);
+		} else {
+			// Device is already registered on GCM, check server.
+			if (GCMRegistrar.isRegisteredOnServer(this)) {
+				// Skips registration.
+				mDisplay.append(getString(R.string.already_registered) + "\n");
+			} else {
+				// Try to register again, but not in the UI thread.
+				// It's also necessary to cancel the thread onDestroy(),
+				// hence the use of AsyncTask instead of a raw thread.
+				final Context context = this;
+				mRegisterTask = new AsyncTask<Void, Void, Void>() {
+
+					@Override
+					protected Void doInBackground(Void... params) {
+						boolean registered = ServerUtilities.register(context,
+								regId);
+						// At this point all attempts to register with the app
+						// server failed, so we need to unregister the device
+						// from GCM - the app will try to register again when
+						// it is restarted. Note that GCM will send an
+						// unregistered callback upon completion, but
+						// GCMIntentService.onUnregistered() will ignore it.
+						if (!registered) {
+							GCMRegistrar.unregister(context);
+						}
+						return null;
+					}
+
+					@Override
+					protected void onPostExecute(Void result) {
+						mRegisterTask = null;
+					}
+
+				};
+				mRegisterTask.execute(null, null, null);
+			}
+		}
+
+		// ----------------------PushEnd-------------------//
+
+		//----------------------TabLayout-----------------//
 		TabHost tabHost = getTabHost();
 
 		// Tab for Job
@@ -101,19 +163,13 @@ public class SkybotTabLayoutActivity extends TabActivity implements
 
 	}
 
-	// -------------------------------------------Push------------------------------------------------------------------
-	// -----------------------------------------------------------------------------------------------------------------
-
-/*	@Override
-	protected void onDestroy() {
-		if (mRegisterTask != null) {
-			mRegisterTask.cancel(true);
-		}
-		unregisterReceiver(mHandleMessageReceiver);
-		GCMRegistrar.onDestroy(this);
-		super.onDestroy();
-	}*/
-
+	// -------------------------------------------Push------------------------------------------//
+	
+	/*
+	 * @Override protected void onDestroy() { if (mRegisterTask != null) {
+	 * mRegisterTask.cancel(true); } unregisterReceiver(mHandleMessageReceiver);
+	 * GCMRegistrar.onDestroy(this); super.onDestroy(); }
+	 */
 
 	private void checkNotNull(Object reference, String name) {
 		if (reference == null) {
@@ -130,12 +186,9 @@ public class SkybotTabLayoutActivity extends TabActivity implements
 		}
 	};
 
-	// --------------------------------------------------------------------------------------------------------
-	// ------------------------------------------End
-	// Push------------------------------------------------------
+	// -------------------------------EndPush------------------------------------//
 
-	// ---------------------------------- Menu Callbacks
-	// -------------------------------//
+	// -----------------------------MenuCallbacks-------------------------------//
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater menuInflater = getMenuInflater();
